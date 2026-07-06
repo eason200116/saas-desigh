836471e879002dfd51e154cdd85992f3515721d6
Eason <eason200116@gmail.com>
2026-07-06 15:06:44 +0400

    feat: 同步原型+4文档最新 + 新增上线 锚点页·全字段词典(docs/migration)

--- prototypes/lotto-v3/assets/web-B2uTlqfq.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./vue-router-CYoKDLBu.js";import{at as t,st as n}from"./main-CMWH9H9a.js";import"./useRouteTabs-DrET2IHM.js";function r(e){console.error(e)}function i(i){let a;i||(a=e());let{push:o,replace:s}=i||a;function c(e=n.BASE_HOME,i=!1){if(e)if(t(e))i?s(e).catch(r):o(e).catch(r);else{let t=e;i?s(t).catch(r):o(t).catch(r)}}return c}export{i as t};
