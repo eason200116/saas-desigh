03f6ca800a0a2426370d3dc07edf8ac9104c00d2
Eason <eason200116@gmail.com>
2026-07-10 20:22:04 +0400

    feat: 商户端原型更新（会员管理：分组/详情/用户批量操作）

---
+++ prototypes/lotto-v3/assets/useBetRecordEmbed-BnxpHwzL.js
@@ -0,0 +1,1 @@
+import{Gn as e,Pn as t}from"./vendor-naive-DfOGXcbR.js";import{_t as n,gt as r}from"./main-DuzHsKBc.js";var i=Symbol(`betRecordEmbed`);function a(t){n(r.global.locale.value,`game`),e(i,t)}function o(){return t(i,void 0)}export{o as n,a as t};
