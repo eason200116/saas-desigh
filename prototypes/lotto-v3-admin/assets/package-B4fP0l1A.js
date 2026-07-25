39fbf443de6bb32950560fd98074c66c0aa5fb1d
Eason <eason200116@gmail.com>
2026-07-25 09:40:41 +0400

    feat: 发布 v2.1.000（target=admin）同步原型+文档最新

--- prototypes/lotto-v3-admin/assets/package-B4fP0l1A.js
+++
@@ -1,1 +0,0 @@
-import{Wn as e,kn as t,xn as n}from"./vendor-naive-DfOGXcbR.js";import{t as r}from"./PackageTab-6shVEA1T.js";var i=t({__name:`index`,setup(t){return(t,i)=>(e(),n(r))}});export{i as default};
--- prototypes/lotto-v3-admin/assets/tenant-routes-CJcQadyB.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./main-CYDzrnBt.js";import{t}from"./role-filter-nBkfuIjQ.js";var n=t(e,`tenant`);export{n as tenantRoutes};
